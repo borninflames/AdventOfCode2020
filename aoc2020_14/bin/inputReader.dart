fe326d3bb829a55bbab344279b91e5638901de4b
Peter <51321635+peterbodizs@users.noreply.github.com>
2020-12-15 17:18:00 +0100

    day 14 part 2 ... finally working

--- aoc2020_14/bin/inputReader.dart
+++ aoc2020_14/bin/inputReader.dart
@@ -28,97 +28,80 @@ class SeaportComputer {
     var instrParts = instruction.split(' = ');
     if (instrParts[0] == 'mask') {
       mask = instruction.split(' = ')[1];
-      print(
-          'New mask---------------------------------------------------------------------------');
     } else {
-      var regex = RegExp(r'\[(\d+)\]');
-      var match = regex.firstMatch(instrParts[0]);
-      if (match != null) {
-        var value = int.parse(instrParts[1]);
-        var nextAddr = getMaskedAddress(match.group(1));
-        print('Raw address:    ${match.group(1)}');
-        print('Masked address: ${nextAddr}');
+      mem = addInstruction(instrParts);
+      print(mem.length);
+    }
+  }
 
-        var memAddresses = mem.keys.toList();
-        //végigmegyek a már meglévő memória címeken
-        memAddresses.forEach((addr) {
-          //distinct
-          if (!areDistinct(addr, nextAddr)) {
-            print('Are they equal: ${addr == nextAddr}');
-            //csökkentjük az eredeti halmazt, az újat is, hogy distinctek legyenek
-            if (addr != nextAddr) {
-              var shrinkedAddr = separate(addr, nextAddr);
-              mem[shrinkedAddr] = mem[addr];
-              mem.remove(addr);
+  Map<String, int> addInstruction(List<String> instrParts) {
+    var memResult = <String, int>{};
+    var regex = RegExp(r'\[(\d+)\]');
+    var match = regex.firstMatch(instrParts[0]);
+    if (match != null) {
+      var value = int.parse(instrParts[1]);
+      var nextAddr = getMaskedAddress(match.group(1));
+
+      var memAddresses = mem.keys.toList();
+      memAddresses.forEach((addr) {
+        if (addr == nextAddr) {
+          memResult[nextAddr] = value;
+        } else if (!areDistinct(addr, nextAddr)) {
+          var address = addr.split('');
+          var changed = false;
+
+          //separate addresses if necessary
+          for (var i = 0; i < addr.length; i++) {
+            if (addr[i] == nextAddr[i] ||
+                addr[i] != 'X' && nextAddr[i] == 'X') {
+              continue;
+            }
+            changed = true;
+
+            var newAddr = List<String>.from(address);
+            newAddr[i] = nextAddr[i] == '1' ? '0' : '1';
+            address[i] = nextAddr[i] == '0' ? '0' : '1';
+
+            //add value to an adress variant
+            if (hasX(newAddr.join(), nextAddr)) {
+              memResult[newAddr.join()] = mem[addr];
             }
           }
-        });
-
-        mem[nextAddr] = value;
-        print(
-            '--------------------------------------------------------------------------------------------');
-      }
+          if (!changed && hasX(addr, nextAddr)) {
+            memResult[addr] = mem[addr];
+          }
+        } else {
+          memResult[addr] = mem[addr];
+        }
+      });
+      memResult[nextAddr] = value;
     }
+
+    return memResult;
   }
 
-  String separate(String addr, String nextAddr) {
-    var shrinkedAddr = '';
-    for (var i = 0; i < addr.length; i++) {
-      if (addr[i] == nextAddr[i]) {
-        shrinkedAddr += addr[i];
-      } else if (addr[i] == 'X' && nextAddr[i] != 'X') {
-        shrinkedAddr += nextAddr[i] == '1' ? '0' : '1';
-      } else if (addr[i] != 'X' && nextAddr[i] == 'X') {
-        shrinkedAddr += addr[i];
+  bool hasX(String shrinkedAddr, String nextAddr) {
+    for (var i = 0; i < nextAddr.length; i++) {
+      if (shrinkedAddr[i] != nextAddr[i] && nextAddr[i] != 'X') {
+        return true;
       }
     }
-
-    print('${addr} was shrinked to be distinct from: ${nextAddr}');
-    print('${shrinkedAddr}');
-    return shrinkedAddr;
-  }
-
-  bool isLargerSet(String addr, String nextAddr) {
-    if (addr == nextAddr) {
-      return true;
-    }
-    var chars = addr.split('');
-    var retVal = true;
-    for (var i = 0; i < chars.length && retVal; i++) {
-      if (chars[i] == 'X' && nextAddr[i] != 'X') {
-        retVal = false;
-        print('isLargerSet: ${retVal} [${i}]');
-      }
-    }
-
-    return retVal;
+    return false;
   }
 
   bool areDistinct(String addr, String nextAddr) {
-    var retVal = false;
-    for (var i = 0; i < addr.length && !retVal; i++) {
-      retVal = addr[i] == '1' && nextAddr[i] == '0' ||
-          addr[i] == '0' && nextAddr[i] == '1';
-    }
-    if (!retVal) {
-      print('Not distinct: ${addr}');
-      print('With this:    ${nextAddr}');
+    for (var i = 0; i < addr.length; i++) {
+      if (addr[i] == '1' && nextAddr[i] == '0' ||
+          addr[i] == '0' && nextAddr[i] == '1') {
+        return true;
+      }
     }
 
-    return retVal;
-  }
-
-  int sumMemory() {
-    var sum = 0;
-    mem.forEach((key, value) {
-      sum += value;
-    });
-    return sum;
+    return false;
   }
 
   int sumMemory2() {
     var sum = 0;
-    print('There are ${mem.length} values');
     mem.forEach((key, value) {
       var exponent = 'X'.allMatches(key).length;
       var a = pow(2, exponent);
@@ -148,26 +131,5 @@ class SeaportComputer {
       }
     }
     return maskedBinary;
-  }
-
-  int getMaskedValue(String val) {
-    var value = int.parse(val);
-    print('Value: ${value}');
-    var binary = value.toRadixString(2);
-    binary = binary.padLeft(36, '0');
-    print('Binary: ${binary}');
-
-    var maskedBinary = '';
-    for (var i = 0; i < mask.length; i++) {
-      if (mask[i] != 'X') {
-        maskedBinary += mask[i];
-      } else {
-        maskedBinary += binary[i];
-      }
-    }
-
-    value = int.parse(maskedBinary, radix: 2);
-    print('Value again: ${value}');
-    return value;
   }
 }
