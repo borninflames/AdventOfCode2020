455d754df5ecbc4dd5558220a603b7b2ab3cc82a
Peter <51321635+peterbodizs@users.noreply.github.com>
2020-12-20 16:11:11 +0100

    day 19 part 2 naah

--- aoc2020_19/bin/inputReader.dart
+++ aoc2020_19/bin/inputReader.dart
@@ -6,7 +6,7 @@ Future<int> readInputFile(String path) async {
   final file = File(path);
   final completer = Completer<int>();
   var inputStream = file.openRead();
-  var rules = List<Rule>(132);
+  var rules = List<Rule>(133);
   var section = 1;
   var messagesToCheck = <String>[];
 
@@ -23,7 +23,7 @@ Future<int> readInputFile(String path) async {
       }
     }
   }, onDone: () {
-    var validMessages = traverse(rules, 0);
+    var validMessages = traverse(rules, 0, 0);
     //print(validMessages);
     completer.complete(countValidMessages(validMessages, messagesToCheck));
   }, onError: (e) {
@@ -40,26 +40,35 @@ int countValidMessages(List<String> validMessages, List<String> messages) {
       .length;
 }
 
-List<String> traverse(List<Rule> rules, int i) {
+List<String> traverse(List<Rule> rules, int i, int level) {
   var rule = rules[i];
+  rule.increaseUsed();
   if (rule.val.isNotEmpty) {
     return [rule.val];
   }
 
   var messages = <String>[];
-
   for (var j = 0; j < rule.rules.length; j++) {
+    if ((rule.id == 8 || rule.id == 11) && rule.isDisabled && j == 1) {
+      break;
+    }
     var messagesToCombine = List<List<String>>(rule.rules[j].length);
-    for (var k = 0; k < rule.rules[j].length; k++) {
+
+    for (var k = 0; j < rule.rules.length && k < rule.rules[j].length; k++) {
       var r = rule.rules[j][k];
-      messagesToCombine[k] = traverse(rules, r);
+      messagesToCombine[k] = traverse(rules, r, level + 1);
+      if ((rule.id == 8 || rule.id == 11) &&
+          rule.isDisabled &&
+          rule.rules.length > 1) {
+        rule.rules.removeLast();
+      }
     }
 
     var combinedMessages = <String>[];
     messagesToCombine[0].forEach((leftMessage) {
       if (messagesToCombine.length > 1) {
         messagesToCombine[1].forEach((rightMessage) {
-          if (messagesToCombine.length > 2) {
+          if (messagesToCombine.length > 2 && messagesToCombine[2] != null) {
             messagesToCombine[2].forEach((rightMessage2) {
               combinedMessages.add(leftMessage + rightMessage + rightMessage2);
             });
@@ -82,6 +91,19 @@ class Rule {
   int id = -1;
   String val = '';
   var rules = <List<int>>[];
+  var isDisabled = false;
+  var usedTimes = 0;
+
+  void increaseUsed() {
+    usedTimes++;
+    if ((id == 8 || id == 11) && usedTimes >= 2) {
+      isDisabled = true;
+      print('Disabled rule: ${id}');
+    }
+    if (usedTimes > 10000) {
+      print('Rule ${id} has been used ${usedTimes} times...');
+    }
+  }
 
   Rule(this.id);
 
--- aoc2020_19/bin/main.dart
+++ aoc2020_19/bin/main.dart
@@ -1,6 +1,7 @@
 import 'inputReader.dart' as input_reader;
 
 void main(List<String> arguments) async {
-  var answer = await input_reader.readInputFile('bin/input.txt');
+  var answer = await input_reader
+      .readInputFile('bin/input2a.txt'); //kézzel egy ciklus beleírva, de szar
   print('The answer is: ${answer}');
 }
