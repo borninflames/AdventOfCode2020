d68c458c91d514bda31843872e61927223e17dbe
Peter <51321635+peterbodizs@users.noreply.github.com>
2020-12-25 21:38:01 +0100

    day 24 part 2

--- aoc2020_24/bin/inputReader.dart
+++ aoc2020_24/bin/inputReader.dart
@@ -6,13 +6,16 @@ Future<int> readInputFile(String path) async {
   final file = File(path);
   final completer = Completer<int>();
   var inputStream = file.openRead();
-  var floor = Floor(101);
+  var floor = Floor(1001);
   var sum = 0;
   inputStream.transform(utf8.decoder).transform(LineSplitter()).listen(
       (String line) {
     sum += floor.flipHexaTile(line);
   }, onDone: () {
-    completer.complete(sum);
+    print(
+        'MinRow: ${floor.minRowNum} MaxRow: ${floor.maxRowNum} MinCol: ${floor.minColNum} MaxCol: ${floor.maxColNum}');
+    print('The PART 1 answer is: ${sum}');
+    completer.complete(floor.daysArePassing(100));
   }, onError: (e) {
     print(e.toString());
   });
@@ -21,6 +24,10 @@ Future<int> readInputFile(String path) async {
 
 class Floor {
   HexaTile refTile;
+  int minRowNum;
+  int minColNum;
+  int maxRowNum;
+  int maxColNum;
   var size = 0;
   List<List<HexaTile>> tiles;
 
@@ -37,6 +44,11 @@ class Floor {
 
     var refPoz = (size / 2).round();
     refTile = tiles[refPoz][refPoz];
+
+    minColNum = size;
+    minRowNum = size;
+    maxColNum = 0;
+    maxRowNum = 0;
   }
 
   int flipHexaTile(String line) {
@@ -87,7 +99,87 @@ class Floor {
     var currentTile = tiles[currRow][currCol];
     currentTile.colour = currentTile.colour == 0 ? 1 : 0;
 
+    maxRowNum = currRow > maxRowNum ? currRow : maxRowNum;
+    minRowNum = currRow < minRowNum ? currRow : minRowNum;
+    maxColNum = currCol > maxColNum ? currCol : maxColNum;
+    minColNum = currCol < minColNum ? currCol : minColNum;
+
     return currentTile.colour == 1 ? 1 : -1;
+  }
+
+  int daysArePassing(int days) {
+    for (var i = 1; i <= days; i++) {
+      tiles = applyRules();
+      print(tiles
+          .expand((tr) => tr.where((t) => t != null && t.colour == 1).toList())
+          .length);
+    }
+
+    var allBlackTiles = tiles
+        .expand((tr) => tr.where((t) => t != null && t.colour == 1).toList());
+    return allBlackTiles.length;
+  }
+
+  List<List<HexaTile>> applyRules() {
+    var t = List<List<HexaTile>>(size);
+    for (var row = 0; row < size; row++) {
+      t[row] = List<HexaTile>(size);
+      for (var col = 0; col < size; col++) {
+        if (row % 2 == 0 && col % 2 == 0 || row % 2 == 1 && col % 2 == 1) {
+          t[row][col] = applyRule(tiles[row][col]);
+        }
+      }
+    }
+    return t;
+  }
+
+  HexaTile applyRule(HexaTile tile) {
+    var adjacent = getAdjacentHexaTiles(tile);
+    var blackAdjacentCount = adjacent.where((t) => t.colour == 1).length;
+    var colour = tile.colour;
+    if (tile.colour == 0 && blackAdjacentCount == 2) {
+      colour = 1;
+    }
+
+    if (tile.colour == 1 &&
+        (blackAdjacentCount == 0 || blackAdjacentCount > 2)) {
+      colour = 0;
+    }
+
+    return HexaTile(tile.row, tile.col, colour);
+  }
+
+  List<HexaTile> getAdjacentHexaTiles(HexaTile tile) {
+    var row = tile.row;
+    var col = tile.col;
+    var adjacentTiles = <HexaTile>[];
+
+    if (col >= 2) {
+      adjacentTiles.add(tiles[row][col - 2]);
+    }
+    if (col < size - 2) {
+      adjacentTiles.add(tiles[row][col + 2]);
+    }
+
+    if (row > 0) {
+      if (col > 0) {
+        adjacentTiles.add(tiles[row - 1][col - 1]);
+      }
+      if (col < size - 1) {
+        adjacentTiles.add(tiles[row - 1][col + 1]);
+      }
+    }
+
+    if (row < size - 1) {
+      if (col > 0) {
+        adjacentTiles.add(tiles[row + 1][col - 1]);
+      }
+      if (col < size - 1) {
+        adjacentTiles.add(tiles[row + 1][col + 1]);
+      }
+    }
+
+    return adjacentTiles;
   }
 }
 
--- aoc2020_24/bin/main.dart
+++ aoc2020_24/bin/main.dart
@@ -2,5 +2,5 @@ import 'inputReader.dart' as input_reader;
 
 void main(List<String> arguments) async {
   var answer = await input_reader.readInputFile('bin/input.txt');
-  print('The PART 1 answer is: ${answer}');
+  print('The PART 2 answer is: ${answer}');
 }
